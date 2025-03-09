0713ac82b205c888be92bf3918151fecc33e3b04
Ranilo <12ranilo3@gmail.com>
2025-03-09 16:04:26 -0300

    feat: fixed input validation to avoid out-of-limit values

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
+const validateInput = (input) => {
+  if (input.value !== "") {
+    let min = parseInt(input.min)
+    let max = parseInt(input.max)
+    let value = parseInt(input.value)
+
+    if ( value < min ) input.value = min
+    if ( value > max ) input.value = max
+  }
+}
+
 const calculateAge = () => {
   const day = document.querySelector(".day").value;
   const month = document.querySelector(".month").value;
   const year = document.querySelector(".year").value;
   const btn = document.querySelector(".btn");
 //   Fazer verificação em cada input
-  if (!day | !month | !year) {
+  if (!day) {
 
   }
 
