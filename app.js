d18f2321711384aba1265d879b0bb1ea12d1b86a
Ranilo <12ranilo3@gmail.com>
2025-03-09 17:21:26 -0300

    add: adding check on empty entries

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
 const validateInput = (input) => {
   if (input.value !== "") {
-    let min = parseInt(input.min)
-    let max = parseInt(input.max)
-    let value = parseInt(input.value)
+    let min = parseInt(input.min);
+    let max = parseInt(input.max);
+    let value = parseInt(input.value);
 
-    if ( value < min ) input.value = min
-    if ( value > max ) input.value = max
+    if (value < min) input.value = min;
+    if (value > max) input.value = max;
   }
-}
+};
 
 const calculateAge = () => {
   const day = document.querySelector(".day").value;
   const month = document.querySelector(".month").value;
   const year = document.querySelector(".year").value;
   const btn = document.querySelector(".btn");
-//   Fazer verificação em cada input
-  if (!day) {
-
+  //   Fazer verificação em cada input
+  if (!day || !month || !year) {
+    document.getElementsByClassName("volumeCheck")[0].style.display = "flex";
+    return;
   }
 
-  btn.addEventListener("click", () => {
-
-  });
+  btn.addEventListener("click", () => {});
 };
