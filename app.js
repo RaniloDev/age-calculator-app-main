d06907a9ca037ffb3029505ac504c6b8ac0b0ad0
Ranilo <12ranilo3@gmail.com>
2025-03-09 15:41:11 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const calculateAge = () => {
+  const day = document.querySelector(".day").value;
+  const month = document.querySelector(".month").value;
+  const year = document.querySelector(".year").value;
+  const btn = document.querySelector(".btn");
+//   Fazer verificação em cada input
+  if (!day | !month | !year) {
+
+  }
+
+  btn.addEventListener("click", () => {
+
+  });
+};
